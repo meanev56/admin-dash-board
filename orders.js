81cc8a249a402b86820856c32ce4ed6f92b5955f
meanev56 <emekanjoku86@gmail.com>
2023-01-28 13:56:16 +0100

    index

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        ProductNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lavender KF102',
+        ProductNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        ProductNumber: '48347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        ProductNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        ProductNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delievered'
+    },
+    {
+        productName: 'DJI Air 25',
+        ProductNumber: '81457',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenga Drone',
+        ProductNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delievered'
+    }
+]
